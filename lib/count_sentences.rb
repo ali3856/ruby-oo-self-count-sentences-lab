6cbbe58d8169042404874ef5d2d18ae8f7188333
Asim Ali <asimsali95@gmail.com>
2021-08-09 15:49:38 -0500

    commit tree 8038dfe482ee5391e6be1273418f68c8bbeb9563

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")#need to look up operators such end_with? and add list to resources 
+      true
+    else 
+      false
+    end  
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    self.squeeze(".?!").count(".?!") # just pairing string class methods together; .squeeze gets rid of multiples (ex. Woo...), count returns count of provided arguements in this case everytime code sees 1 .!?
   end
 end
+
+#refer to yellow notepad for all "built in" methods of ruby
